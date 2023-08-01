ca3e1f738b5c5b76b363f1e976879540dbce901b
Alejo <jubany4@gmail.com>
2023-08-01 16:54:23 -0300

    haciendo deploy back

--- api/src/db.js
+++ api/src/db.js
@@ -1,19 +1,15 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
-} = process.env;
-
-const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-dialectOptions:{
-  ssl:{
-    require:true,
-  }
-}});
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
+const sequelize = new Sequelize(
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+    {
+        logging: false, // set to console.log to see the raw SQL queries
+        native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    }
+);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
